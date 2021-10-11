793706819d3d0f65af1a3e32a73e44aef9ae6f06
Murat Altay <murat.altay@akinon.com>
2021-10-11 13:09:15 +0300

    🛠  main [MODIFIED] index page

--- pages/index.js
+++ pages/index.js
@@ -11,38 +11,15 @@ export default function Home() {
 
       <main>
         <h1 className='title'>
-          Welcome to <a href='https://nextjs.org'>Next.js!</a>
+          Welcome to <a href='https://nextjs.org'>Pokedex!</a>
         </h1>
-
-        <p className='description'>
-          Get started by editing <code>pages/index.js</code>
-        </p>
 
         <div className='grid'>
           <Link href='/list'>
             <a className='card'>
               <h3>Pokemon List &rarr;</h3>
-              <p>Find in-depth information about Next.js features and API.</p>
             </a>
           </Link>
-
-          <a href='https://nextjs.org/learn' className='card'>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a href='https://github.com/vercel/next.js/tree/master/examples' className='card'>
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href='https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app'
-            className='card'
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>Instantly deploy your Next.js site to a public URL with Vercel.</p>
-          </a>
         </div>
       </main>
 
